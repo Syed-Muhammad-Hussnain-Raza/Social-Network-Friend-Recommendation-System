1e35d679b9d93e5de616b565b18ac9bb30b18cd3
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-11-20 00:42:27 +0500

    Main.java - A test file from dev branch as default

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("From 'dev branch' - \"default\"");
         System.out.println("Social Network Friend Recommendation System");
     }
 }
