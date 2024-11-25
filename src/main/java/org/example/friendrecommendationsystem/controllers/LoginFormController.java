2d94505b1ca690aa3865e0e98aac1d95aa0d3e5d
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-11-26 03:06:21 +0500

    LoginFormController class added here to handle LoginForm.fxml

---
+++ src/main/java/org/example/friendrecommendationsystem/controllers/LoginFormController.java
@@ -0,0 +1,29 @@
+package org.example.friendrecommendationsystem.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+
+    @FXML
+    private CheckBox captchaBox;
+
+    @FXML
+    private Button forgotPasswordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private AnchorPane usernameTextField;
+
+}
