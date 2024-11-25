132843f8b78e31eb1d5c4e5a8982b0cf998eee78
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-11-25 18:05:18 +0500

    Updting my project structure

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("From 'dev branch' - \"default\"");
-        System.out.println("Social Network Friend Recommendation System");
-    }
-}
