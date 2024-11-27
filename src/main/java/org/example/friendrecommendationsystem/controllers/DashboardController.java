2bd3e0f81fe597b0c9a6fd389428d481941de97b
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-11-27 09:33:03 +0500

    Controller class for Dashboard of application

---
+++ src/main/java/org/example/friendrecommendationsystem/controllers/DashboardController.java
@@ -0,0 +1,32 @@
+package org.example.friendrecommendationsystem.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class DashboardController {
+
+    @FXML
+    private Button addFriendButton;
+
+    @FXML
+    private Button createPostsButton;
+
+    @FXML
+    private Button displayAllFriendsButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button recommendFriendsButton;
+
+    @FXML
+    private Label userMailLabel;
+
+    @FXML
+    private Label usernameLabel;
+
+
+
+}
