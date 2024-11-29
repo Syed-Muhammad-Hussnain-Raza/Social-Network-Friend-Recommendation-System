98d8045938d62489177750fe779b7fdc1da56f94
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-11-29 21:42:18 +0500

    SQL added in info of application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.friendrecommendationsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.friendrecommendationsystem to javafx.fxml;
