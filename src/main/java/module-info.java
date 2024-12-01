1f4d0e8016945801d63f9c4b421012ccb6124e7d
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-12-01 17:07:13 +0500

    Module Info updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.friendrecommendationsystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens org.example.friendrecommendationsystem to javafx.fxml;
