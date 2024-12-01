478b2be8d7375a7633f21af940e1d6c559a26507
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-12-01 13:00:43 +0500

    Database connected - on pressing submit button, It store user's data in database

--- src/main/java/org/example/friendrecommendationsystem/controllers/RegistrationController.java
+++ src/main/java/org/example/friendrecommendationsystem/controllers/RegistrationController.java
@@ -9,9 +9,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.example.friendrecommendationsystem.database.DatabaseConnection;
 import org.example.friendrecommendationsystem.utilities.SwitchScene;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RegistrationController {
 
@@ -65,5 +69,43 @@ public class RegistrationController {
         // Load the Registration form
         Stage currentStage = ( Stage ) signInButton.getScene().getWindow();
         SwitchScene.changeScene(currentStage, "/views/LoginForm.fxml", "Login Form");
+    }
+
+    @FXML
+    private void handleSubmitButton() {
+        String username = nameField.getText();
+        String password = passwordField.getText();
+        String gender = genderBox.getValue();
+        String dob = dobPicker.getValue() != null ? dobPicker.getValue().toString() : null;
+        String address = addressField.getText();
+        String about = aboutMe.getText();
+
+        if (username.isEmpty() || password.isEmpty() || gender == null
+                || dob == null || address.isEmpty() || about.isEmpty()) {
+            System.out.println("All fields are required!");
+            return;
+        }
+
+        String insertQuery = "INSERT INTO users(username, password, dob, gender, address, email) VALUE(?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = new DatabaseConnection().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, dob);
+            preparedStatement.setString(4, gender);
+            preparedStatement.setString(5, address);
+            preparedStatement.setString(6, about);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Registration successful!");
+                handleSignInButton();
+            } else {
+                System.out.println("Failed to register.");
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
