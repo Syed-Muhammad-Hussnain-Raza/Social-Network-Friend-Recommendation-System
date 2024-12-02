1b7abdc01f7461c74c98b58f421ae62c8a162174
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-12-02 21:27:30 +0500

    Dashboard form updated

--- src/main/java/org/example/friendrecommendationsystem/controllers/DashboardController.java
+++ src/main/java/org/example/friendrecommendationsystem/controllers/DashboardController.java
@@ -3,8 +3,9 @@ package org.example.friendrecommendationsystem.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import org.example.friendrecommendationsystem.utilities.SwitchScene;
+import org.example.friendrecommendationsystem.utilities.Utils;
 
 public class DashboardController {
 
@@ -27,15 +28,32 @@ public class DashboardController {
     private Label userMailLabel;
 
     @FXML
+    private AnchorPane rightPane;
+
+    @FXML
     private Label usernameLabel;
 
     public void initialize() {
         logoutButton.setOnAction(event -> handleLogoutButton());
+        createPostsButton.setOnAction(event -> handleCreatePostButton());
+//        usernameLabel.setText(currentUser);
     }
 
     private void handleLogoutButton() {
         Stage currentStage = ( Stage ) logoutButton.getScene().getWindow();
-        SwitchScene.changeScene(currentStage, "/views/LoginForm.fxml", "Login Form");
+        Utils.changeScene(currentStage, "/views/LoginForm.fxml", "Login Form");
         System.out.println("Logout button clicked");
+    }
+
+    private void handleAddFriend() {
+        System.out.println("Logout button clicked");
+    }
+
+    private void handleCreatePostButton() {
+//        Stage currentStage = (Stage) createPostsButton.getScene().getWindow();
+//        Utils.openNewWindow(currentStage, "/views/CreatePostForm.fxml", "Create Post");
+//        System.out.println("Create Post button clicked");
+
+        Utils.loadAnchorPaneContent(rightPane, "/views/CreatePostForm.fxml");
     }
 }
