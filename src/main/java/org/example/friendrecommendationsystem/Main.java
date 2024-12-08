ff57cc38a1346529e4d688aa694719790a21336e
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-12-08 18:39:08 +0500

    Main Class updated

--- src/main/java/org/example/friendrecommendationsystem/Main.java
+++ src/main/java/org/example/friendrecommendationsystem/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load());
 
         scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
-
+        
         stage.setTitle("Login Form");
         stage.setScene(scene);
         stage.show();
